47374181134e2202c1d1c49e807bf13eeb611144
bekbol-bek <rysbekovbekbolin@gmail.com>
2024-11-20 22:07:54 +0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,28 +1,24 @@
-"""
-URL configuration for myshop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
 
-from shop.views import index
-
+from .views import *
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('shop.urls'))
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('car/<slug:catid>/', views.car, name='car'),
+    path('', views.index, name='index'),
+
+    path('Ecosystems/', views.Ecosystem_categories, name='Ecosystem_categories'),
+    path('Ecosystems/<slug:slug>/', views.Ecosystem_by_category, name='Ecosystem_by_category'),
+
+
+
+
+    path('taxi', views.taxi, name='taxi'),
+    path('about/', views.about, name='about'),
+    path('login', views.login ,name='login'),
+    path('accounts/', include('allauth.urls')),
+    path('tours/', views.tour_categories, name='tour_categories'),
+    path('tours/<str:category_id>/', views.tours_by_category, name='tours_by_category'),
+
+    # Другие маршруты
+
+]
